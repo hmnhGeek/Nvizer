984b1996367189cb90bb141dff112eaae14b062a
hmnhGeek <hmnhsharma97@gmail.com>
2024-05-26 14:15:34 +0530

    using cors anywhere to bypass cors

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,10 +1,28 @@
 import axios from "axios";
 import { conf } from "../conf/conf";
 
-const Api = (url, method = "get", data = null, forceUrl = false) => {
+const Api = (
+  url,
+  method = "get",
+  data = null,
+  forceUrl = false,
+  proxyEnabled = true
+) => {
+  if (!forceUrl && !proxyEnabled) {
+    url = conf.apiBaseUrl + url;
+  } else if (!forceUrl && proxyEnabled) {
+    url = `${conf.proxyUrl}/${conf.apiBaseUrl}${url}`;
+  } else if (forceUrl && !proxyEnabled) {
+    // nothing needs to be done here.
+  } else {
+    url = `${conf.proxyUrl}/${url}`;
+  }
+
+  console.log("Calling url:", url);
+
   return axios({
     method,
-    url: forceUrl ? url : conf.apiBaseUrl + url,
+    url,
     data,
   });
 };
