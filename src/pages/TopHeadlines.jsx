8fc94ef2b4319499d61317f3adbf871b08e6482c
hmnhGeek <hmnhsharma97@gmail.com>
2024-06-02 11:08:24 +0530

    Custom template for top headlines

--- src/pages/TopHeadlines.jsx
+++ src/pages/TopHeadlines.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import GeneralHeadlines from "../components/TopHeadlines/GeneralHeadlines";
+import ScienceHeadlines from "../components/TopHeadlines/ScienceHeadlines";
 
 const TopHeadlines = () => {
   return (
     <>
       <div className="flex overflow-x-auto whitespace-nowrap">
         <GeneralHeadlines />
+      </div>
+      <div className="flex overflow-x-auto whitespace-nowrap">
+        <ScienceHeadlines />
       </div>
     </>
   );
