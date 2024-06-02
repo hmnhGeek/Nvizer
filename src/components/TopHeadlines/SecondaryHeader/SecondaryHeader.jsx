4b2e2bce0181fe3be9647ebbaec5205302cd6334
hmnhGeek <hmnhsharma97@gmail.com>
2024-06-02 12:53:39 +0530

    Code refactoring on secondary header

--- src/components/TopHeadlines/SecondaryHeader/SecondaryHeader.jsx
+++ src/components/TopHeadlines/SecondaryHeader/SecondaryHeader.jsx
@@ -5,82 +5,31 @@ import { NavLink } from "react-router-dom";
 
 function SecondaryHeader(props) {
   const [hamMenu, setHamMenu] = useState("hidden");
+  const categories = [
+    "general",
+    "world",
+    "nation",
+    "business",
+    "technology",
+    "entertainment",
+    "sports",
+    "science",
+    "health",
+  ];
 
   return (
     <nav className="p-3 flex bg-gray-100 justify-center items-center">
       <div className="hidden md:flex gap-12">
-        <NavLink
-          to="/top-headlines/general"
-          className={({ isActive }) =>
-            `font-medium ${isActive ? "text-primary" : ""} hover:text-primary`
-          }
-        >
-          General
-        </NavLink>
-        <NavLink
-          to="/top-headlines/world"
-          className={({ isActive }) =>
-            `font-medium ${isActive ? "text-primary" : ""} hover:text-primary`
-          }
-        >
-          World
-        </NavLink>
-        <NavLink
-          to="/top-headlines/nation"
-          className={({ isActive }) =>
-            `font-medium ${isActive ? "text-primary" : ""} hover:text-primary`
-          }
-        >
-          Nation
-        </NavLink>
-        <NavLink
-          to="/top-headlines/business"
-          className={({ isActive }) =>
-            `font-medium ${isActive ? "text-primary" : ""} hover:text-primary`
-          }
-        >
-          Business
-        </NavLink>
-        <NavLink
-          to="/top-headlines/technology"
-          className={({ isActive }) =>
-            `font-medium ${isActive ? "text-primary" : ""} hover:text-primary`
-          }
-        >
-          Technology
-        </NavLink>
-        <NavLink
-          to="/top-headlines/entertainment"
-          className={({ isActive }) =>
-            `font-medium ${isActive ? "text-primary" : ""} hover:text-primary`
-          }
-        >
-          Entertainment
-        </NavLink>
-        <NavLink
-          to="/top-headlines/sports"
-          className={({ isActive }) =>
-            `font-medium ${isActive ? "text-primary" : ""} hover:text-primary`
-          }
-        >
-          Sports
-        </NavLink>
-        <NavLink
-          to="/top-headlines/science"
-          className={({ isActive }) =>
-            `font-medium ${isActive ? "text-primary" : ""} hover:text-primary`
-          }
-        >
-          Science
-        </NavLink>
-        <NavLink
-          to="/top-headlines/health"
-          className={({ isActive }) =>
-            `font-medium ${isActive ? "text-primary" : ""} hover:text-primary`
-          }
-        >
-          Health
-        </NavLink>
+        {categories.map((category) => (
+          <NavLink
+            to={`/top-headlines/${category}`}
+            className={({ isActive }) =>
+              `font-medium ${isActive ? "text-primary" : ""} hover:text-primary`
+            }
+          >
+            {category.charAt(0).toUpperCase() + category.slice(1)}
+          </NavLink>
+        ))}
       </div>
       <button className="flex md:hidden" onClick={() => setHamMenu("")}>
         <FontAwesomeIcon className="text-gray-600" icon={faBars} />
@@ -96,105 +45,19 @@ function SecondaryHeader(props) {
           </button>
         </div>
         <div className="mt-6 flex flex-col items-center">
-          <NavLink
-            to="/top-headlines/general"
-            onClick={() => setHamMenu("hidden")}
-            className={({ isActive }) =>
-              `font-medium m-3 p-3 block rounded-lg hover:bg-gray-50 ${
-                isActive ? "text-primary" : ""
-              }`
-            }
-          >
-            General
-          </NavLink>
-          <NavLink
-            to="/top-headlines/world"
-            onClick={() => setHamMenu("hidden")}
-            className={({ isActive }) =>
-              `font-medium m-3 p-3 block rounded-lg hover:bg-gray-50 ${
-                isActive ? "text-primary" : ""
-              }`
-            }
-          >
-            World
-          </NavLink>
-          <NavLink
-            to="/top-headlines/nation"
-            onClick={() => setHamMenu("hidden")}
-            className={({ isActive }) =>
-              `font-medium m-3 p-3 block rounded-lg hover:bg-gray-50 ${
-                isActive ? "text-primary" : ""
-              }`
-            }
-          >
-            Nation
-          </NavLink>
-          <NavLink
-            to="/top-headlines/business"
-            onClick={() => setHamMenu("hidden")}
-            className={({ isActive }) =>
-              `font-medium m-3 p-3 block rounded-lg hover:bg-gray-50 ${
-                isActive ? "text-primary" : ""
-              }`
-            }
-          >
-            Business
-          </NavLink>
-          <NavLink
-            to="/top-headlines/technology"
-            onClick={() => setHamMenu("hidden")}
-            className={({ isActive }) =>
-              `font-medium m-3 p-3 block rounded-lg hover:bg-gray-50 ${
-                isActive ? "text-primary" : ""
-              }`
-            }
-          >
-            Technology
-          </NavLink>
-          <NavLink
-            to="/top-headlines/entertainment"
-            onClick={() => setHamMenu("hidden")}
-            className={({ isActive }) =>
-              `font-medium m-3 p-3 block rounded-lg hover:bg-gray-50 ${
-                isActive ? "text-primary" : ""
-              }`
-            }
-          >
-            Entertainment
-          </NavLink>
-          <NavLink
-            to="/top-headlines/sports"
-            onClick={() => setHamMenu("hidden")}
-            className={({ isActive }) =>
-              `font-medium m-3 p-3 block rounded-lg hover:bg-gray-50 ${
-                isActive ? "text-primary" : ""
-              }`
-            }
-          >
-            Sports
-          </NavLink>
-          <NavLink
-            to="/top-headlines/science"
-            onClick={() => setHamMenu("hidden")}
-            className={({ isActive }) =>
-              `font-medium m-3 p-3 block rounded-lg hover:bg-gray-50 ${
-                isActive ? "text-primary" : ""
-              }`
-            }
-          >
-            Science
-          </NavLink>
-          <NavLink
-            to="/top-headlines/health"
-            onClick={() => setHamMenu("hidden")}
-            className={({ isActive }) =>
-              `font-medium m-3 p-3 block rounded-lg hover:bg-gray-50 ${
-                isActive ? "text-primary" : ""
-              }`
-            }
-          >
-            Health
-          </NavLink>
+          {categories.map((category) => (
+            <NavLink
+              to={`/top-headlines/${category}`}
+              onClick={() => setHamMenu("hidden")}
+              className={({ isActive }) =>
+                `font-medium m-3 p-3 block rounded-lg hover:bg-gray-50 ${
+                  isActive ? "text-primary" : ""
+                }`
+              }
+            >
+              {category.charAt(0).toUpperCase() + category.slice(1)}
+            </NavLink>
+          ))}
         </div>
       </div>
     </nav>
