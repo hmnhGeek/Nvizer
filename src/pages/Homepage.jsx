63a75ff41a21403be8587126fcd3c1f4e8387ee0
hmnhGeek <hmnhsharma97@gmail.com>
2024-06-08 15:05:08 +0530

    New homepage look

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,12 +6,12 @@ import NewsSearchBar from "../components/HomeHeadlines/NewsSearchBar";
 const Homepage = () => {
   return (
     <>
-      {/* <SourcesSlider /> */}
       <div className="w-full px-8 pt-8 lg:px-96">
         <NewsSearchBar />
       </div>
       <div className="h-[1px] bg-gray-300 m-8"></div>
-      {/* <HomeHeadlines /> */}
+      <HomeHeadlines />
+      {/* <SourcesSlider /> */}
     </>
   );
 };
