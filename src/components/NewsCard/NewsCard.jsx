ac9cd88183d246172554b9e1c6b91d8ba51ffba3
hmnhGeek <hmnhsharma97@gmail.com>
2024-06-15 17:05:11 +0530

    added favourite icon for logged in users

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,10 +1,12 @@
 import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faRegularStar } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
-const NewsCard = ({ news, onHover, onLeave }) => {
+const NewsCard = ({ news, onHover, onLeave, isFavourite = false }) => {
   const token = useSelector((state) => state.auth.token);
+  const [isFav, setIsFav] = useState(isFavourite);
 
   const truncateText = (text, maxLength) => {
     if (text.length > maxLength) {
@@ -24,8 +26,7 @@ const NewsCard = ({ news, onHover, onLeave }) => {
 
   return (
     <div
-      className="max-w-sm rounded overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 cursor-pointer m-2"
-      onClick={() => window.open(news.url, "_blank")}
+      className="max-w-sm rounded overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 m-2"
       onMouseEnter={onHover}
       onMouseLeave={onLeave}
       style={{ height: "22.75rem" }} // Set a fixed height
@@ -40,15 +41,20 @@ const NewsCard = ({ news, onHover, onLeave }) => {
           {news.source.name}
         </span>
         {token && (
-          <span className="absolute top-2 left-2 text-yellow-500 text-lg">
+          <span
+            className={`absolute top-2 left-2 cursor-pointer text-yellow-500 bg-black text-lg rounded-full py-2 px-3`}
+          >
             <FontAwesomeIcon
-              // className="text-gray-600 hover:text-red-800"
-              icon={faStar}
+              icon={isFav ? faStar : faRegularStar}
+              onClick={() => setIsFav((x) => !x)}
             />
           </span>
         )}
       </div>
-      <div className="p-4">
+      <div
+        onClick={() => window.open(news.url, "_blank")}
+        className="p-4 cursor-pointer"
+      >
         <h2
           className="font-bold text-xl mb-2 overflow-hidden whitespace-nowrap"
           title={news.title}
